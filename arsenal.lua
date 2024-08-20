5716739d16b738cd286e411cc6e50b89b2e3f210
Sxm19J <155818064+Sxm19J@users.noreply.github.com>
2024-08-20 22:19:02 +0100

    Create arsenal.lua

---
+++ arsenal.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/DarkNetworks/Infinite-Yield/main/latest.lua'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NebulaServicesA/AppleWare/main/Arsenal"))()
